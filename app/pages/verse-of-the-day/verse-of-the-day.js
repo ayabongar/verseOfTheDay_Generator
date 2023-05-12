ca8bebff866dc14441242a3e588bd07a84d9226d
Adam Kaminski <adamka@bbd.co.za>
2023-05-12 19:49:16 +0200

    Event handling for like and share button

--- app/pages/verse-of-the-day/verse-of-the-day.js
+++ app/pages/verse-of-the-day/verse-of-the-day.js
@@ -1,1 +1,65 @@
-console.log("Verse of the day works");
+const introText = "Here's the verse of the day!\n"
+
+let main = document.getElementsByTagName("main")[0];
+let likeButton = document.getElementsByClassName("like")[0];
+let likeClicked = false;
+let shareButton = document.getElementsByClassName("share")[0];
+
+const shareData = {
+    title: "MDN",
+    text: "Learn web development on MDN!",
+    url: "https://developer.mozilla.org",
+  };
+
+likeButton.addEventListener("click",()=>{
+    if(!likeClicked){
+        likeButton.classList.add("click");
+        //send req to api to update db
+        likeClicked = !likeClicked;
+    }else{
+        likeButton.classList.remove("click");
+        //send req to api to update db
+        likeClicked = !likeClicked;
+    }
+})
+
+shareButton.addEventListener("click", (e)=>{
+    let verseContainer = document.getElementsByClassName("verse")[0];
+    let title = verseContainer.getElementsByTagName('h1')[0];
+    let body = verseContainer.getElementsByTagName('blockquote')[0];
+
+    let text = `${introText}${title.textContent}\n${body.textContent}`;
+    navigator.clipboard.writeText(text);
+    navigator.share({
+        title:title,
+        text:body
+    })
+
+    // popup(e, "Copied to clipboard!", 2);
+})
+
+function popup(e,text, duration){
+    //Get position of cursor
+    let y = e.clientY-8;
+    let x = e.clientX+30;
+    
+    let popupText = document.createElement("dialog");
+    
+    popupText.classList.add("popup");
+    popupText.textContent = text;
+    popupText.style.opacity = 1;
+    popupText.style.position = "absolute";
+    popupText.style.top = y+"px";
+    popupText.style.left = x+"px";
+    
+    main.appendChild(popupText)
+    
+    let interval = setInterval(()=>{
+        popupText.style.opacity = popupText.style.opacity - 0.02;
+    },10)
+
+    let timeout = setTimeout(()=>{
+        main.removeChild(popupText);
+        clearInterval(interval);
+    },800);
+}
