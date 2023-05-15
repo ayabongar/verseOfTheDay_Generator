2e890f9774198d66a47d73f898108046ed0d2e09
Mikhail Govender <mikhailg@bbd.co.za>
2023-05-15 11:04:10 +0200

    Update Server Routing for Images, Fix Link in HTML pages and remove unnessary Folder

--- server.js
+++ server.js
@@ -4,35 +4,44 @@ const url = require('url');
 const _path = require('path');
 //installed MIME type library to avoid manually setting content types
 const mimeTypesLookup = require('mime-types').lookup;
+//Make Folder Name Global so that it does not change each time server runs
+let folderName = '';
 
 
 const server = http.createServer((request, response) => {
 
     //Define Variables
     let file = '';
-    let folderName = '';
     let mimeType = '';
+    let filePath = '';
 
     //Handle Requests and Send Static Files
     let _URL = url.parse(request.url);
-
     //strip leading and trailing slashes
     let path = _URL.path.replace(/^\/+|\/+$/g,"");
 
     if(path == '' || path == 'login'){
         path = 'login.html';
     }
-    //Resolve Folder and MIME Type
-    folderName = handleFolderName(path);
-    mimeType = mimeTypesLookup(path);
 
-    if(fs.existsSync(__dirname + `/app/pages/${folderName}/`+path))
+    if(path != undefined)
     {
-        file = __dirname + `/app/pages/${folderName}/`+path;
+        if(path.includes('.') && !path.includes('Images/'))
+        {
+            folderName = path.slice(0, path.indexOf('.'));
+        }
+    }
+
+    mimeType = mimeTypesLookup(path);
+    filePath = __dirname + `/app/pages/${folderName}/`+path;
+
+    if(fs.existsSync(filePath))
+    {
+        file = filePath;
         console.log('Fetching Resources - ' + file);
     }else
     {
-        console.log('Path Does Not Exist - ' + __dirname + `/app/pages/${folderName}/`+path);
+        console.log('Path Does Not Exist - ' + filePath);
         file = __dirname + `/app/pages/error/error.html`;
     }
     
@@ -47,25 +56,8 @@ const server = http.createServer((request, response) => {
             response.end(content);
         }
     })
-    
 });
 
-function handleFolderName(path){
-    if(path.includes('.html'))
-    {
-        return path.replace('.html','');
-            }
-    else if(path.includes('.css'))
-    {
-       return path.replace('.css','');
-                }
-    else if(path.includes('.js'))
-    {
-       return path.replace('.js','');
-                }
-
-    return '';
-    }
 
 server.listen(8080, 'localhost', () => {
     console.log('Server is listening on port 8080');
