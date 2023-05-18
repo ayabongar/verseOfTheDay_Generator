7118e30c4b14d56dc163371f4157b1c28ef0ddce
Mikhail Govender <mikhailg@bbd.co.za>
2023-05-18 14:13:07 +0200

    Implement User Login and Registration with Hashing Updated Old Server

--- server.js
+++ server.js
@@ -1,105 +1,89 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const _path = require('path');
-const UserLogin = require('./UserLogin');
-const { type } = require('os');
-const mimeTypesLookup = require('mime-types').lookup;
-let folderName = '';
-let LoginSuccess = false;
+const express = require('express');
+const app = express();
+const port = process.env.port || 8080;
+const path = require('path');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const login_handler = require('./UserLogin');
+const reg_handler = require('./UserRegistration');
 
 
-const server = http.createServer((request, response) => {
-    let file = '';
-    let mimeType = '';
-    let filePath = '';
-    let body = '';
 
-    let _URL = url.parse(request.url);
-    let path = _URL.path.replace(/^\/+|\/+$/g,"");
+app.use(session({
+    secret: 'secret',
+    resave : true,
+    saveUninitialized : true
+}))
+app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname,'/views')));
+app.use(express.static(path.join(__dirname,'/views/javascript')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-    if(path == ''){
-        path = 'login.html';
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+})
+
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+})
+
+app.get('/verseoftheday', (req,res) =>{
+    if(req.session.loggedin){
+        res.sendFile(path.join(__dirname, '/views/verse-of-the-day.html'));
+    }else{
+        res.redirect('/');
     }
+})
 
-    if(path != undefined)
-    {
-        if(path.includes('.') && !path.includes('Images/'))
-        {
-            folderName = path.slice(0, path.indexOf('.'));
+app.get('/verseoftheweek', (req,res) =>{
+    if(req.session.loggedin){
+        res.sendFile(path.join(__dirname, '/views/verse-of-the-week.html'));
+    }else{
+        res.redirect('/');
+    }
+})
+
+app.post('/registerUser', (req,res) =>{
+    let username = req.body.username;
+    let password = req.body.password;
+    if(username && password){
+        //TODO - SANITIZE TEXT
+        try{
+            reg_handler.RegisterUser(username,password)
+            .then(result =>{
+                console.log('user registered successfully');
+                res.redirect('/');
+            })
+        }catch(error){
+            res.status(500).send();
         }
     }
-    
-    // if(path == 'UserLogin')
-    // {
-    //     let user = '';
-    //     console.log('User Attempting To Login...');
-    //     const { headers, method, url } = request;
-    //     request.on('error', (err) => {
-    //       console.error(err);
-    //     }).on('data', (chunk) => {
-    //       body += (chunk);
-    //     }).on('end', () => {
-    //       body = JSON.parse(body);
-    //       console.log(body);//IDK why but i need this to access the props??
-    //       console.log(body.username);
-    //       console.log(body.password);
 
-    //       user = {username: body.username, password: body.password};
-    //       console.log(user);
+})
 
-    //     UserLogin.GetUserByUserName(user.username)
-    //     .then( (val) =>{
-
-    //         console.log(val);
-
-    //         let dbResult = val[0];
-
-    //         console.log(dbResult.username);
-    //         console.log(dbResult.password);
-
-    //         if(body.username == dbResult.username)
-    //         {
-    //             if(body.password == dbResult.password)
-    //             {
-    //                 console.log('USER NAME AND PASSWORD MATCH..');
-    //                 LoginSuccess = true;
-    //             }
-    //         }
-    //     })
-    //     .catch(err => console.log(err));
-    //     });
-    // }
-
-
-
-    mimeType = mimeTypesLookup(path);
-
-    console.log(filePath);
-
-    if(fs.existsSync(filePath))
-    {
-        file = filePath;
-    }else
-    {
-        file = __dirname + `/app/pages/error/error.html`;
+app.post('/auth', (req,res) =>{
+    let username = req.body.username;
+    let password = req.body.password;
+    if(username && password){
+        login_handler.VerifyLogin(username,password)
+        .then(result => {
+            if(result){
+                console.log('user login succcess');
+                req.session.loggedin = true;
+                req.session.username = result.username;
+                res.redirect('/verseoftheday')
+            }else{
+                res.json({success: 'fail'});
+            }
+        });
     }
+})
 
+app.listen(port, err =>{
+    if(err){
+        return console.log(err);
+    }
+    console.log(`Listening On ${port}`);
     
-    fs.readFile(file, function(err, content){
-        if(err){
-            console.log(err);
-                response.writeHead(404);
-                response.end();
-        } else {
-            console.log(`Returning Path ${path}`);
-            response.writeHead(200, {'Content-type':mimeType});
-            response.end(content);
-        }
-    })
-});
-
-
-server.listen(8080, 'localhost', () => {
-    console.log('Server is listening on port 8080');
-});
+})
