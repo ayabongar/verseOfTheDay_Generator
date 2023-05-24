8d02cf064b0b3087e670841a91dd76f24e6ca67c
Kyle Pottinger <kylep@bbd.co.za>
2023-05-24 20:42:32 +0200

    feature: Added loader to login and register

--- views/javascript/login.js
+++ views/javascript/login.js
@@ -1,15 +1,19 @@
-// const form = document.querySelector('form');
-// // const username = document.getElementById('username');
-// // const password = document.getElementById('password');
+const form = document.querySelector('form');
+// const username = document.getElementById('username');
+// const password = document.getElementById('password');
+const loginButton = document.getElementById('login-button');
+const loader = document.getElementById('login-loader');
+
+form.addEventListener('submit', (e) => {
+    loginButton.classList.add('hidden');
+    loader.classList.remove('hidden');
 
 
-// form.addEventListener('submit', (e) => {
-
-//     fetch('/auth')
-//     .then( function(response){
-//         console.log(response.json());
-//     }).then( function(text){
-//         alert(text);
-//     }).catch( (err)=>{alert(err)});
+    fetch('/auth')
+    .then( function(response){
+        console.log(response.json());
+    }).then( function(text){
+        alert(text);
+    }).catch( (err)=>{alert(err)});
     
-// });
+});
--- views/javascript/register.js
+++ views/javascript/register.js
@@ -1,1 +1,8 @@
-console.log("Register works");
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    const loginButton = document.getElementById('register-button');
+    const loader = document.getElementById('register-loader');
+    loginButton.classList.add('hidden');
+    loader.classList.remove('hidden');
+});
